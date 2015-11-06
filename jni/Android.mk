a33b311a45dd5f6fa6607aa7f1ce58c746106b97
Jan Weitz <jan@iosphere.de>
2015-11-07 00:00:48 +0100

    Geos 3.5 Upgrade. Rename makefiles

--- jni/Android.mk
+++ jni/Android.mk
@@ -12,22 +12,16 @@ LOCAL_MODULE := spatial
 ICONV_PATH := libiconv-1.13.1
 SQLITE_PATH := sqlite-amalgamation-3090200
 PROJ4_PATH := proj.4-4.9.2
-GEOS_PATH := geos-3.4.2
+GEOS_PATH := geos-3.5.0
 LZMA_PATH := xz-5.2.2
 XML2_PATH := libxml2-2.9.1
 SPATIALITE_PATH := libspatialite
 
-# SQLITE_PATH := sqlite-autoconf-3090200
-# include $(LOCAL_PATH)/sqlite-autoconf-3090200.mk
-#
-# SQLITE_PATH := sqlite-amalgamation-3081002
-# include $(LOCAL_PATH)/sqlite-3081002.mk
-#
 include $(LOCAL_PATH)/sqlite-amalgamation-3090200.mk
 include $(LOCAL_PATH)/libiconv-1.13.1.mk
 include $(LOCAL_PATH)/proj.4-4.9.2.mk
-include $(LOCAL_PATH)/geos-3.4.2.mk
+include $(LOCAL_PATH)/geos-3.5.0-CAPI-1.9.0_r4084.mk
 include $(LOCAL_PATH)/xz-5.2.2.mk
 include $(LOCAL_PATH)/libxml2-2.9.1.mk
-include $(LOCAL_PATH)/spatialite-4.3.0a_so.mk
+include $(LOCAL_PATH)/spatialite-4.4.0-topo-1.so.mk
 $(call import-module,android/cpufeatures)
