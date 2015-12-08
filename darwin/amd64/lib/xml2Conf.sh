293fb481cecf1ad943c561ee26455dea2a36a4db
Jan Weitz <jan@iosphere.de>
2015-12-08 22:27:04 +0100

    New binaries

--- darwin/amd64/lib/xml2Conf.sh
+++ darwin/amd64/lib/xml2Conf.sh
@@ -2,7 +2,7 @@
 # Configuration file for using the XML library in GNOME applications
 #
 XML2_LIBDIR="-L/Users/weitz/Documents/work/spatial/darwin/amd64/lib"
-XML2_LIBS="-lxml2 -lz -lpthread  -liconv -lm "
-XML2_INCLUDEDIR="-I/Users/weitz/Documents/work/spatial/darwin/amd64/include/libxml2"
+XML2_LIBS="-lxml2 -lz -lpthread -L/Users/weitz/Documents/work/spatial/darwin/amd64/lib -liconv -lm "
+XML2_INCLUDEDIR="-I/Users/weitz/Documents/work/spatial/darwin/amd64/include/libxml2 -I/Users/weitz/Documents/work/spatial/darwin/amd64/include"
 MODULE_VERSION="xml2-2.9.1"
 
