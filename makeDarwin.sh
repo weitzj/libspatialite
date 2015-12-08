8a50815664e30df2dd27518e6bf30a080e08c028
Jan Weitz <jan@iosphere.de>
2015-12-08 22:14:59 +0100

    Adds iconv, zlib

--- makeDarwin.sh
+++ makeDarwin.sh
@@ -7,7 +7,8 @@ PREFIX="${BASE}/darwin/amd64"
 INCLUDEDIR="${PREFIX}/include"
 LIBDIR="${PREFIX}/lib"
 
-ICONV_PATH="${JNIDIR}/libiconv-1.13.1"
+ZLIB_PATH="${JNIDIR}/zlib-1.2.8"
+ICONV_PATH="${JNIDIR}/libiconv-1.14"
 SQLITE_PATH="${JNIDIR}/sqlite-autoconf-3090200"
 PROJ4_PATH="${JNIDIR}/proj.4-4.9.2"
 GEOS_PATH="${JNIDIR}/geos-3.5.0"
@@ -15,10 +16,37 @@ LZMA_PATH="${JNIDIR}/xz-5.2.2"
 XML2_PATH="${JNIDIR}/libxml2-2.9.1"
 SPATIALITE_PATH="${JNIDIR}/libspatialite"
 
-
 # SQLITE
 cd "${SQLITE_PATH}"
-env CFLAGS='-DSQLITE_ENABLE_LOAD_EXTENSION' ./configure --prefix="${PREFIX}" --enable-json1 --disable-static
+env CFLAGS='-DSQLITE_ENABLE_LOAD_EXTENSION' ./configure --prefix="${PREFIX}" --disable-static --enable-json1
+make
+make install
+make distclean
+
+# ZLIB
+cd "${ZLIB_PATH}"
+./configure --prefix="${PREFIX}"
+make
+make install
+make distclean
+
+# ICONV
+cd "${ICONV_PATH}"
+./configure --prefix="${PREFIX}" --disable-static
+make
+make install
+make distclean
+
+# XZ
+cd "${LZMA_PATH}"
+./configure --prefix="${PREFIX}" --disable-static --with-libiconv-prefix="${PREFIX}"
+make
+make install
+make distclean
+
+# LIBXML2
+cd "${XML2_PATH}"
+./configure --prefix="${PREFIX}" --disable-static --with-lzma="${PREFIX}" --with-iconv="${PREFIX}"
 make
 make install
 make distclean
@@ -37,29 +65,15 @@ make
 make install
 make distclean
 
-# XZ
-cd "${LZMA_PATH}"
-./configure --prefix="${PREFIX}" --disable-static
-make
-make install
-make distclean
-
-# LIBXML2
-cd "${XML2_PATH}"
-./configure --prefix="${PREFIX}" --disable-static
-make
-make install
-make distclean
-
+# Spatialite
+export CFLAGS="-I${INCLUDEDIR}"
+export LDFLAGS="-L${LIBDIR}"
 cd "${SPATIALITE_PATH}"
-export CFLAGS="-I${PREFIX}/include"
-export LDFLAGS="-L${PREFIX}/lib"
 ./configure --prefix="${PREFIX}" --enable-module-only --with-geosconfig="${PREFIX}/bin/geos-config" --disable-examples --disable-freexl
 make
 make install
 make distclean
 cp "${LIBDIR}/mod_spatialite.7.so" "${LIBDIR}/mod_spatialite.dylib"
-
 
 # Fixing dynamic library search paths
 for fullPath in $(find $LIBDIR -type f -maxdepth 1 -regex ".*[.dylib]\$")
@@ -70,13 +84,23 @@ do
     then
         install_name_tool -change "${LIBDIR}/libgeos-3.5.0.dylib" "@loader_path/libgeos-3.5.0.dylib" "${fullPath}"
     fi
+    if [ "${fileName}" == 'libxml2.2.dylib' ]
+    then
+        install_name_tool -change "${LIBDIR}/libz.1.dylib" "@loader_path/libz.1.dylib" "${fullPath}"
+        install_name_tool -change "${LIBDIR}/liblzma.5.dylib" "@loader_path/liblzma.5.dylib" "${fullPath}"
+        install_name_tool -change "${LIBDIR}/libiconv.2.dylib" "@loader_path/libiconv.2.dylib" "${fullPath}"
+    fi
     if [ "${fileName}" == 'mod_spatialite.dylib' ]
     then
-        echo "fixing libspatialite"
         install_name_tool -change "${LIBDIR}/libgeos_c.1.dylib" "@loader_path/libgeos_c.1.dylib" "${fullPath}"
         install_name_tool -change "${LIBDIR}/libgeos-3.5.0.dylib" "@loader_path/libgeos-3.5.0.dylib" "${fullPath}"
         install_name_tool -change "${LIBDIR}/libproj.9.dylib" "@loader_path/libproj.9.dylib" "${fullPath}"
         install_name_tool -change "${LIBDIR}/libsqlite3.0.dylib" "@loader_path/libsqlite3.0.dylib" "${fullPath}"
         install_name_tool -change "${LIBDIR}/libxml2.2.dylib" "@loader_path/libxml2.2.dylib" "${fullPath}"
+        install_name_tool -change "${LIBDIR}/liblzma.5.dylib" "@loader_path/liblzma.5.dylib" "${fullPath}"
+        install_name_tool -change "${LIBDIR}/libcharset.1.dylib" "@loader_path/libcharset.1.dylib" "${fullPath}"
+        install_name_tool -change "${LIBDIR}/libiconv.2.dylib" "@loader_path/libiconv.2.dylib" "${fullPath}"
+        install_name_tool -change "${LIBDIR}/libz.1.dylib" "@loader_path/libz.1.dylib" "${fullPath}"
     fi
 done
+
